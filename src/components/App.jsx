1421e7bc701d3af528e11f9831384fdc8d6870d9
podletsky <nazar.org@gmail.com>
2023-06-15 17:21:43 +0300

    progresing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackOption from '../components/feedback/FeedbackOption';
+import Statistic from './statistic/Statistic';
+import SectionTitle from './sectionTitle/SectionTitle';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  }
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option.toLowerCase()]: prevState[option.toLowerCase()] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+
+    return (
+      <>
+        <FeedbackOption
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+
+        {good === 0 && neutral === 0 && bad === 0 ? (
+          <SectionTitle>No feedback yet.</SectionTitle>
+        ) : (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            positiveFeedback={Math.round((good / total) * 100)}
+            total={total}
+          />
+        )}
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/sectionTitle/SectionTitle.jsx
@@ -0,0 +1,7 @@
+import styles from '../Feedback.module.css';
+
+const SectionTitle = () => {
+  return <h2 className={styles.title}>There is no feedback</h2>;
+};
+
+export default SectionTitle;
