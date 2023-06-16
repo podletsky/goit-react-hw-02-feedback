53bc47af3dea8eaf06b47ec50a5aed5ff3f51195
podletsky <nazar.org@gmail.com>
2023-06-16 13:02:47 +0300

    amendments to the work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,40 +4,44 @@ import Statistic from './statistic/Statistic';
 import SectionTitle from './sectionTitle/SectionTitle';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
   onLeaveFeedback = option => {
     this.setState(prevState => ({
-      [option.toLowerCase()]: prevState[option.toLowerCase()] + 1,
+      [option]: prevState[option] + 1,
     }));
   };
-
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return Math.round((this.state.good / total) * 100);
+  };
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
   render() {
     const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
+    const total = this.countTotalFeedback();
 
     return (
       <>
         <FeedbackOption
-          options={['Good', 'Neutral', 'Bad']}
+          options={Object.keys(this.state)}
           onLeaveFeedback={this.onLeaveFeedback}
         />
 
-        {good === 0 && neutral === 0 && bad === 0 ? (
+        {!total ? (
           <SectionTitle>No feedback yet.</SectionTitle>
         ) : (
           <Statistic
             good={good}
             neutral={neutral}
             bad={bad}
-            positiveFeedback={Math.round((good / total) * 100)}
+            positiveFeedback={this.countPositiveFeedbackPercentage()}
             total={total}
           />
         )}
